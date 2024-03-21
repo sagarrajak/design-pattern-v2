72eef7916b8e351f714349274df001499f765c16
sagar <sagarrajak858@gmail.com>
2024-03-21 19:02:11 +0530

    fix:

---
+++ src/main/java/com/fancystore/behavioural/mediator/ChatRoomExample.java
@@ -0,0 +1,11 @@
+package com.fancystore.behavioural.mediator;
+
+import java.util.List;
+
+public class ChatRoomExample {
+}
+
+class Person {
+    private String name;
+    private List<String> chatLog;
+}
